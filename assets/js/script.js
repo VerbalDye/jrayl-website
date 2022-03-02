eee18e3645a18e63b4c3e35144eb927e9976f722
Dustin Huntsberger <dhunts258@gmail.com>
2022-03-02 17:04:13 -0500

    more styling to header and hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,8 @@
-function animateIcon() {
-    let storeBar = document.getElementById("bar2");
-    let isOpen = window.getComputedStyle(storeBar).getPropertyValue("opacity");
-    if (isOpen == 1) {
-    document.getElementById("bar1").style.transform = "translateY(.6rem) rotate(45deg)";
-    document.getElementById("bar2").style.opacity = "0";
-    document.getElementById("bar3").style.transform = "translateY(-.6rem) rotate(-45deg)";
-    document.getElementById("menu").style.height = "100vh";
-    document.getElementById("menuIcon").style.transform = "rotate(180deg)";
-    }
-    if (isOpen == 0) {
-      document.getElementById("bar1").style.transform = "translateY(0rem) rotate(0deg)";
-      document.getElementById("bar2").style.opacity = "1";
-      document.getElementById("bar3").style.transform = "translateY(0rem) rotate(0deg)";
-      document.getElementById("menu").style.height = "0vh";
-      document.getElementById("menuIcon").style.transform = "rotate(0deg)";
-    }
-  }
-  
-  function closeMenu() {
-    document.getElementById("bar1").style.transform = "translateY(0rem) rotate(0deg)";
-    document.getElementById("bar2").style.opacity = "1";
-    document.getElementById("bar3").style.transform = "translateY(0rem) rotate(0deg)";
-    document.getElementById("menu").style.height = "0vh";
-    document.getElementById("menuIcon").style.transform = "rotate(0deg)";
-  }
+var driverDrop = document.getElementById("driverDrop");
+var dropDown = document.getElementById("dropDown");
+driverDrop.addEventListener("mouseover", function() {
+    dropDown.style.opacity = "1";
+});
+driverDrop.addEventListener("mouseout", function() {
+    dropDown.style.opacity = "0";
+});
