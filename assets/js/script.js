6dc62c2b83566f7745b532462948b517fef55d19
Dustin Huntsberger <dhunts258@gmail.com>
2022-03-04 09:42:28 -0500

    submission box and about us blerb

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
 var driverDrop = document.getElementById("driverDrop");
 var dropDown = document.getElementById("dropDown");
+var onDrop = false;
+var onDriver = false;
+
 driverDrop.addEventListener("mouseover", function() {
-    dropDown.style.opacity = "1";
+    onDriver = true;
+    showOrNo("driverOn");
 });
 driverDrop.addEventListener("mouseout", function() {
-    dropDown.style.opacity = "0";
+    onDriver = false;
+    showOrNo("driverOut");
 });
+
+dropDown.addEventListener("mouseover", function() {
+    onDrop = true;
+    showOrNo("dropOn");
+});
+dropDown.addEventListener("mouseout", function() {
+    onDrop = false;
+    showOrNo("dropOut");
+});
+
+var showOrNo = function(condition) {
+    if (onDrop || onDriver) {
+        dropDown.style.opacity = "1";
+    } else {
+        dropDown.style.opacity = "0";
+    }
+};
+
