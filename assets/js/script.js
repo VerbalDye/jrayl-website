7bfa445dcdd02211b1362792b49fa4a585e1eb45
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-01 11:03:10 -0400

    fixed hero and header using clever js and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
-//Resize Header
+var headerEl = document.querySelector("header");
 var heroVideo = document.getElementById("heroVideo");
-
-window.addEventListener("resize", resizeHero);
+var heroEl = document.querySelector(".hero");
 
 var resizeHero = function() {
-    var windowHeight = window.innerHeight + "px";
-    var windowWidth = window.innerHeight + "px";
-    heroVideo.style.height = windowHeight;
-    heroVideo.style.minWidth = windowWidth;
-    heroVideo.style.bottom = windowHeight;
-    heroVideo.style.top = "none"
-    console.log("Success");
+    var headerHeight = headerEl.offsetHeight;
+    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth;
+
+    heroEl.style.height = windowHeight - headerHeight + "px";
+
+    console.log(headerHeight);
+    console.log(windowHeight);
+    console.log(headerHeight - windowHeight);
+    console.log(heroEl.style.height);
+
+    if (windowHeight/1080 > windowWidth/1920) {
+        console.log("long");
+        heroVideo.style.width = "";
+        heroVideo.style.height = windowHeight + "px";
+        var heroWidth = heroVideo.clientWidth;
+        heroVideo.style.top = "0";
+        heroVideo.style.left = -(heroWidth - windowWidth) / 2 + "px";
+    } else {
+        console.log("wide");
+        heroVideo.style.height = "";
+        heroVideo.style.width = windowWidth + "px";
+        var heroHeight = (windowWidth / 16) * 9;
+        console.log(heroHeight);
+        heroVideo.style.left = "0";
+        heroVideo.style.top = -(heroHeight - windowHeight) + "px";
+    }
 };
 
+var setStyle = function(element, style, value) {
+
+}
+
 resizeHero();
+
+onresize = resizeHero;
